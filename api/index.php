ec679c60ebd8049f5a9581b21f2b1629529db3cc
ZSCGR <932478692@qq.com>
2024-04-08 16:36:00 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
+/*Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);*/
+TimeHelper::secondEndToday();
 
-throw new RuntimeException('Hello Tracy!');
+/*throw new RuntimeException('Hello Tracy!');*/
