8db3a3bfb893622b1990ae30922557544ee9356d
ZSCGR <932478692@qq.com>
2024-04-08 16:52:38 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,11 +1,2 @@
 <?php
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use zjkal/time-helper\TimeHelper;
-
-/*Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);*/
-$weekDay = TimeHelper::getWeekDay();
-echo "当前星期几：$weekDay\n";
-
-/*throw new RuntimeException('Hello Tracy!');*/
+phpinfo();
