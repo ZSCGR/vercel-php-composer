99c5c260c7db39bccfef1b8ddc311f98178760c3
ZSCGR <932478692@qq.com>
2024-04-08 15:46:04 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,6 +2,25 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-Tracy\Debugger::enable(Tracy\Debugger::DEVELOPMENT);
+use jkal\TimeHelper;
 
-throw new RuntimeException('Hello Tracy!');
+/* 获取当前秒级的时间戳 */
+$timestamp = TimeHelper::getTimestamp();
+echo "当前秒级时间戳：$timestamp\n";
+
+/* 获取当前毫秒级的时间戳 */
+$milliTimestamp = TimeHelper::getMilliTimestamp();
+echo "当前毫秒级时间戳：$milliTimestamp\n";
+
+/* 获取今天的秒级时间戳结束时间 */
+$secondEndToday = TimeHelper::secondEndToday();
+echo "今天的秒级时间戳结束时间：$secondEndToday\n";
+
+/* 获取当前星期几 */
+$weekDay = TimeHelper::getWeekDay();
+echo "当前星期几：$weekDay\n";
+
+/* 检查今年是否是闰年 */
+$isLeapYear = TimeHelper::isLeapYear();
+echo "今年是否是闰年：$isLeapYear\n";
+
