10eeb834659a09c6d2cff6d6be0fb820588ba6a5
ZSCGR <932478692@qq.com>
2024-04-08 17:18:32 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Carbon\Carbon;
 
-printf("Right now is %s", Carbon::now()->tz('Asia/Shanghai')->toDateTimeString());
+printf("现在时间是： %s", Carbon::now()->tz('Asia/Shanghai')->toDateTimeString());
