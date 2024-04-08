291e0cf7c8820691eb9cea04d253572a3954be5b
ZSCGR <932478692@qq.com>
2024-04-08 17:12:54 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Carbon\Carbon;
-use zjkal\time-helper
+//use zjkal\time-helper
 
 printf("Right now is %s", Carbon::now()->toDateTimeString());
 //printf("Right now is %s", TimeHelper::secondEndToday());
