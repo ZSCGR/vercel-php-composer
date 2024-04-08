c46144d09b01ffcb94c39fdacc7f24833f49b2fa
ZSCGR <932478692@qq.com>
2024-04-08 17:05:40 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -5,45 +5,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Carbon\Carbon;
 
 printf("Right now is %s", Carbon::now()->toDateTimeString());
-$tomorrow = Carbon::now()->addDay();
-$lastWeek = Carbon::now()->subWeek();
 
-$officialDate = Carbon::now()->toRfc2822String();
-
-$howOldAmI = Carbon::createFromDate(1975, 5, 21)->age;
-
-$noonTodayLondonTime = Carbon::createFromTime(12, 0, 0, 'Asia/Shanghai');
-
-$internetWillBlowUpOn = Carbon::create(2038, 01, 19, 3, 14, 7, 'GMT');
-
-// Don't really want this to happen so mock now
-Carbon::setTestNow(Carbon::createFromDate(2000, 1, 1));
-
-// comparisons are always done in UTC
-if (Carbon::now()->gte($internetWillBlowUpOn)) {
-    die();
-}
-
-// Phew! Return to normal behaviour
-Carbon::setTestNow();
-
-if (Carbon::now()->isWeekend()) {
-    echo 'Party!';
-}
-// Over 200 languages (and over 500 regional variants) supported:
-echo Carbon::now()->subMinutes(2)->diffForHumans(); // '2 minutes ago'
-echo Carbon::now()->subMinutes(2)->locale('zh_CN')->diffForHumans(); // '2分钟前'
-echo Carbon::parse('2019-07-23 14:51')->isoFormat('LLLL'); // 'Tuesday, July 23, 2019 2:51 PM'
-echo Carbon::parse('2019-07-23 14:51')->locale('fr_FR')->isoFormat('LLLL'); // 'mardi 23 juillet 2019 14:51'
-
-// ... but also does 'from now', 'after' and 'before'
-// rolling up to seconds, minutes, hours, days, months, years
-
-$daysSinceEpoch = Carbon::createFromTimestamp(0)->diffInDays(); // something such as:
-                                                                // 19817.6771
-$daysUntilInternetBlowUp = $internetWillBlowUpOn->diffInDays(); // Negative value since it's in the future:
-                                                                // -5037.4560
-
-// Without parameter, difference is calculated from now, but doing $a->diff($b)
-// it will count time from $a to $b.
-Carbon::createFromTimestamp(0)->diffInDays($internetWillBlowUpOn); // 24855.1348
